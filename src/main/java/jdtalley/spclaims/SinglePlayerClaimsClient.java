15e5a404b5703b68ec93f02663cec3009aa90c55
Dakota Talley <jdtalley16@gmail.com>
2023-11-20 12:25:54 -0500

    Initial commit

---
+++ src/main/java/jdtalley/spclaims/SinglePlayerClaimsClient.java
@@ -0,0 +1,10 @@
+package jdtalley.spclaims;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SinglePlayerClaimsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/jdtalley/spclaims/SinglePlayerClaimsDataGenerator.java
@@ -0,0 +1,11 @@
+package jdtalley.spclaims;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SinglePlayerClaimsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
